0681a7b50c767dc79f5f178f20b4ce1d20e5781e
Guido Draheim <guidod@gmx.de>
2024-04-08 17:47:25 +0200

    functionality handled by similar section above

--- unicoder.py
+++ unicoder.py
@@ -488,16 +488,6 @@ def power(text: str) -> str:
                 out.write(chr(norm_super_after[c]))
             else:
                 power = False
-                if c in norm_power_signs:
-                    if x + 1 < len(text) and (text[x + 1] in norm_super_numbers or text[x + 1] in norm_super_before):
-                        power = True
-                        continue  # drop the power sign
-                    if x + 1 < len(text) and (norm_base_a <= ord(text[x + 1]) and ord(text[x + 1]) <= norm_base_z):
-                        graec = ""  # graec += text[x+1] # in next loop
-                        continue
-                    if x + 1 < len(text) and (norm_base_A <= ord(text[x + 1]) and ord(text[x + 1]) <= norm_base_Z):
-                        graec = ""  # graec += text[x+1] # in next loop
-                        continue
                 out.write(c)
         else:
             out.write(c)
